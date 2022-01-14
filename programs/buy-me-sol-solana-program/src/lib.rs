4b3e92d09d4a44b7e0ca01e8244a3c25e9ae4ab1
Pervez Shoaib <50441829+PervezSH@users.noreply.github.com>
2022-01-14 16:48:21 +0530

    feat (solana program) : add methods to create account ✨
    - initialize base account
    - add create creator account method
    - add create supporter account method

--- programs/buy-me-sol-solana-program/src/lib.rs
+++ programs/buy-me-sol-solana-program/src/lib.rs
@@ -6,9 +6,99 @@ declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 pub mod buy_me_sol_solana_program {
     use super::*;
     pub fn initialize(ctx: Context<Initialize>) -> ProgramResult {
+        // Get reference of the base account
+        let base_account = &mut ctx.accounts.base_account;
+        // Initialize creators
+        base_account.total_creators = 0;
+        base_account.total_supporters = 0;
+        Ok(())
+    }
+
+    // Function to create creator account
+    pub fn create_creator(
+        ctx: Context<CreateAccount>,
+        username: String,
+        name: String,
+    ) -> ProgramResult {
+        // Get reference of the base account
+        let base_account = &mut ctx.accounts.base_account;
+        let user = &mut ctx.accounts.user;
+
+        // BUild the struct
+        let creator = CreatorStruct {
+            user_address: *user.to_account_info().key,
+            username: username.to_string(),
+            name: name.to_string(),
+        };
+
+        // Add it to the creator_list vector
+        base_account.creator_list.push(creator);
+        // Update Creator Count
+        base_account.total_creators += 1;
+
+        Ok(())
+    }
+
+    // Function to create supporter account
+    pub fn create_supporter(ctx: Context<CreateAccount>, name: String) -> ProgramResult {
+        // Get reference of the base account
+        let base_account = &mut ctx.accounts.base_account;
+        let user = &mut ctx.accounts.user;
+
+        // BUild the struct
+        let supporter = SupporterStruct {
+            user_address: *user.to_account_info().key,
+            name: name.to_string(),
+        };
+
+        // Add it to the creator_list vector
+        base_account.supporter_list.push(supporter);
+        // Update Creator Count
+        base_account.total_supporters += 1;
+
         Ok(())
     }
 }
 
+// Specify data needed in Initialize Context
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct Initialize<'info> {
+    #[account(init, payer = user, space = 10000)]
+    pub base_account: Account<'info, BaseAccount>,
+    #[account(mut)]
+    pub user: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+// Specify data needed in CreateAccount Context
+#[derive(Accounts)]
+pub struct CreateAccount<'info> {
+    #[account(mut)]
+    pub base_account: Account<'info, BaseAccount>,
+    #[account(mut)]
+    pub user: Signer<'info>,
+}
+
+// Custom struct for Creator Account
+#[derive(Debug, Clone, AnchorSerialize, AnchorDeserialize)]
+pub struct CreatorStruct {
+    pub user_address: Pubkey,
+    pub username: String,
+    pub name: String,
+}
+
+// Custom struct for Supporter Account
+#[derive(Debug, Clone, AnchorSerialize, AnchorDeserialize)]
+pub struct SupporterStruct {
+    pub user_address: Pubkey,
+    pub name: String,
+}
+
+#[account]
+pub struct BaseAccount {
+    pub total_creators: u64,
+    pub total_supporters: u64,
+    // Vector of type CreatorStruct to store creators list
+    pub creator_list: Vec<CreatorStruct>,
+    pub supporter_list: Vec<SupporterStruct>,
+}
--- tests/buy-me-sol-solana-program.js
+++ tests/buy-me-sol-solana-program.js
@@ -1,13 +1,60 @@
 const anchor = require('@project-serum/anchor');
 
+const { SystemProgram } = anchor.web3;  
+
 const main = async() => {
   console.log("🚀 Starting test...")
 
-  anchor.setProvider(anchor.Provider.env());
+  // Create provide
+  const provider = anchor.Provider.env();
+  // Set provider
+  anchor.setProvider(provider);
   const program = anchor.workspace.BuyMeSolSolanaProgram;
-  const tx = await program.rpc.initialize();
 
+  // Create account keypair for our program to use
+  const baseAccount = anchor.web3.Keypair.generate();
+
+  // Call Initialize, pass params it needs
+  let tx = await program.rpc.initialize({
+    accounts: {
+      baseAccount: baseAccount.publicKey,
+      user: provider.wallet.publicKey,
+      systemProgram: SystemProgram.programId,
+    },
+    signers: [baseAccount],
+  });
+  
   console.log("📝 Your transaction signature", tx);
+  
+  // Fetch data from account
+  let account = await program.account.baseAccount.fetch(baseAccount.publicKey);
+  console.log('👀 Total Creators: ', account.totalCreators.toString());
+  console.log('👀 Total Supporters: ', account.totalSupporters.toString());
+  
+  // Call create creator account
+  await program.rpc.createCreator("Apratim Mehta", "apraX568",{
+    accounts: {
+      baseAccount: baseAccount.publicKey,
+      user: provider.wallet.publicKey,
+    }
+  });
+
+  // Call create creator account
+  await program.rpc.createSupporter("Apratim Mehta", {
+    accounts: {
+      baseAccount: baseAccount.publicKey,
+      user: provider.wallet.publicKey,
+    }
+  });
+
+  account = await program.account.baseAccount.fetch(baseAccount.publicKey);
+  console.log('👀 Total Creators: ', account.totalCreators.toString());
+  console.log('👀 Total Supporters: ', account.totalSupporters.toString());
+
+  // Access creator_list on the account
+  console.log('👀 Creator List :', account.creatorList);
+  // Access creator_list on the account
+  console.log('👀 Supporter List :', account.supporterList);
 }
 
 const runMain = async () => {
